bbe9abe3160c3ff02e4a69428afb81c17164596f
DESKTOP-I2OVG7C\Drake <kaiden3991@gmail.com>
2017-10-24 16:52:07 +0200

    removed unnecessary files added index.php files with redirects to the root php file in all subdirectories directories under root

---
+++ www/asset/include/index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Drake
+ * Date: 24.10.2017
+ * Time: 15:51
+ */
+header("Location: ../../index.php");
--- www/asset/include/phpinfo.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Drake
- * Date: 22.10.2017
- * Time: 14:43
- */
-phpinfo();
